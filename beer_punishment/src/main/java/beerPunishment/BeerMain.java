f21b150422002b452d9a13cacfff241cff4dc159
mauricew <mauricew@stud.ntnu.no>
2022-09-16 14:27:25 +0200

    Implemented write/read to File

--- beer_punishment/src/main/java/beerPunishment/BeerMain.java
+++ beer_punishment/src/main/java/beerPunishment/BeerMain.java
@@ -2,10 +2,16 @@ package beerPunishment;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.BinaryOperator;
-import java.util.function.UnaryOperator;
 
 public class BeerMain {
     
+    private List<Rule> rules = new ArrayList<>();
 
+    public BeerMain(List<Rule> rules) {
+        this.rules = rules;
+    }
+
+    public List<Rule> getRules() {
+        return rules;
+    }
 }

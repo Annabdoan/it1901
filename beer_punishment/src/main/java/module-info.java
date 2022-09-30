2c8cbe5018ca503a7b00d868ce6e74d053c43afb
leaandersenn <89526348+leaandersenn@users.noreply.github.com>
2022-09-30 10:50:57 +0200

    Fix: Deleted some lines in module-info.java in order for app to launch.

--- beer_punishment/src/main/java/module-info.java
+++ beer_punishment/src/main/java/module-info.java
@@ -2,8 +2,6 @@ module beerPunishment {
     requires javafx.controls;
     requires javafx.fxml;
 
-    opens beerPunishment to javafx.graphics, javafx.fxml;
     opens beerPunishment.ui to javafx.fxml, javafx.graphics;
-    opens beerPunishment.core to javafx.fxml, javafx.graphics;
-    opens beerPunishment.json to javafx.fxml, javafx.graphics;
+
 }

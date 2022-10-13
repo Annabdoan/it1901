6c41d7fc66d4a74c20d891596eca336ea2b7c9b6
mauricew <mauricew@stud.ntnu.no>
2022-10-13 19:50:04 +0200

    Fix: Updated controller Co-Authored-By: Lea leaj@stud.ntnu.no Co-Authored-By: Anna abdoan@stud.ntnu.no

--- beer_punishment/json/src/main/java/beerPunishment/json/RulePersistence.java
+++ beer_punishment/json/src/main/java/beerPunishment/json/RulePersistence.java
@@ -5,9 +5,7 @@ import beerPunishment.core.Rule;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.*;
-import java.nio.file.FileSystemLoopException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+
 
 
 public class RulePersistence {
@@ -16,22 +14,21 @@ public class RulePersistence {
      */
 
     private ObjectMapper mapper = new ObjectMapper().registerModule(createModule());
-    private Path filePath;
-    private String fileName;
     private File ruleFile;
-    private BeerMain beerMain = new BeerMain();
+    private BeerMain beerMain;
 
     public RulePersistence() {
-        ruleFile = newFile("ruleFile.json");
+        ruleFile = newFile("ruleSaveFile.json");
     }
 
-    public void mockPrint() {
-        System.out.println("wahoo");
+    public File newFile(String filename) {
+        return new File(filename);
     }
 
     public RuleModule createModule() {
         return new RuleModule();
     }
+
     public void createFile(String fileName) throws IOException {
         this.ruleFile = newFile(fileName);
         if (newFile(fileName).createNewFile()) {
@@ -41,22 +38,17 @@ public class RulePersistence {
         }
     }
 
-    public File newFile(String filename) {
-        return new File(filename);
+    public Rule readRule(File file) throws IOException {
+        return mapper.readValue(file, Rule.class);
+    }
+
+    public BeerMain readBeerMain(File file) throws IOException {
+        return mapper.readValue(file, BeerMain.class);
     }
 
 
-    public Rule readRule(File reader) throws IOException {
-        return mapper.readValue(reader, Rule.class);
-    }
-
-    public BeerMain readBeerMain(File reader) throws IOException {
-        return mapper.readValue(reader, BeerMain.class);
-    }
-
-
-    public void writeRule(BeerMain ruleList, String filename) throws IOException {
-        mapper.writeValue(ruleFile, ruleList);
+    public void writeBeerMain(BeerMain beerMain, File file) throws IOException {
+        mapper.writeValue(file, beerMain);
     }
 
     public static void main(String[] args) throws IOException {
@@ -78,9 +70,9 @@ public class RulePersistence {
         ruleList.punishMember("Anna", rule2);
         ruleList.punishMember("Maurice", rule1);
         System.out.println(ruleList);
-        rulepersistence.writeRule(ruleList, fileName);
-        BeerMain beerMain3 = rulepersistence.readBeerMain(rulepersistence.ruleFile);
-        beerMain3.addMember("Lea");
-        System.out.println(beerMain3);
+        //rulepersistence.writeRule(ruleList, fileName);
+        //BeerMain beerMain3 = rulepersistence.readBeerMain(rulepersistence.ruleFile);
+        //beerMain3.addMember("Lea");
+        //System.out.println(beerMain3);
     }
 }
--- beer_punishment/ui/src/main/java/beerPunishment/ui/BeerController.java
+++ beer_punishment/ui/src/main/java/beerPunishment/ui/BeerController.java
@@ -3,6 +3,8 @@ package beerPunishment.ui;
 //import java.awt.*;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -36,13 +38,35 @@ public class BeerController {
     public Button punishButton;
     public ListView punishmentStatusOverview;
     public TextField addMemberText;
-    private File ruleFile = new File("ruleTest.json");
+    private Path filePath;
     @FXML
-    public void initialize() throws Exception{
+    public void initialize() {
         rulePersistence = new RulePersistence();
-        beermain = new BeerMain();
-        System.out.println("h");
-        rulePersistence.mockPrint();
+        this.setFilePath("beersystem.json");
+        try {
+            System.out.println(filePath.toString());
+            beermain = rulePersistence.readBeerMain(new File(filePath.toString()));
+            updateMemberView();
+            updatePersonChoicebox();
+            updateListView();
+            updateRuleChoicebox();
+        } catch (IOException ioe) {
+            beermain = new BeerMain();
+            try {
+                System.out.println("Jeg gikk inn her jeg");
+                rulePersistence.createFile(filePath.toString());
+            }
+            catch (IOException ioe3) {
+                showErrorMessage("Noe feil skjedde i ioe3");
+            }
+
+            /*try {
+                new File(filePath.toString()).createNewFile();
+            }catch (IOException ioe2) {
+                showErrorMessage("feilet å lage ny fil");
+            }*/
+            showErrorMessage("Feil ved initialize");
+        }
 
 /*
         try {
@@ -54,6 +78,10 @@ public class BeerController {
         }
         */
 
+    }
+
+    private void setFilePath(String filename) {
+        this.filePath = Paths.get(System.getProperty("user.home"), filename);
     }
 
     @FXML
@@ -69,13 +97,12 @@ public class BeerController {
     private void updateRuleChoicebox() {
         List<String> ruleDescpritions = new ArrayList<>();
         try {
-            List<Rule> rules = filehandler.readRules(filename);
-            for (Rule rule : rules) {
+            for (Rule rule : beermain.getRules()) {
                 ruleDescpritions.add(rule.getDescription());
             }
             ruleChoiceBox.getItems().setAll(ruleDescpritions);
         } catch (Exception e) {
-            showErrorMessage("Feil ved lesing fra fil");
+            showErrorMessage("Feil updateRuleChoiceBox");
         }
     }
 
@@ -102,11 +129,11 @@ public class BeerController {
             String[] arrOfNewRuleTextInput = newRuleTextInput.getText().split(";");
             Rule rule = new Rule(arrOfNewRuleTextInput[0], Integer.valueOf(arrOfNewRuleTextInput[1]));
             beermain.addRule(rule);
-            filehandler.writeRule(filename,rule);
+            rulePersistence.writeBeerMain(beermain, new File(filePath.toString()));
             updateListView();
             updateRuleChoicebox();
         } catch (Exception e) {
-            showErrorMessage(e.getMessage());
+            showErrorMessage("Feil i make new rule");
         }
 
     }
@@ -123,6 +150,11 @@ public class BeerController {
         for (Rule rule: beermain.getRules()) {
             if (rule.getDescription().equals(chosenRule)) {
                 beermain.punishMember(chosenMember,rule);
+                try {
+                    rulePersistence.writeBeerMain(beermain, new File(filePath.toString()));
+                }catch (IOException punishMemberIOe) {
+                    showErrorMessage("Failed to punish member");
+                }
             }
         }
         updateMemberView();
@@ -133,6 +165,11 @@ public class BeerController {
         String username = addMemberText.getText();
         try {
             beermain.addMember(username);
+            try {
+                rulePersistence.writeBeerMain(beermain, new File(filePath.toString()));
+            }catch (IOException addMemberIOe) {
+                showErrorMessage("Failed to add member");
+            }
             updateMemberView();
             updatePersonChoicebox();
         }catch (IllegalArgumentException e) {

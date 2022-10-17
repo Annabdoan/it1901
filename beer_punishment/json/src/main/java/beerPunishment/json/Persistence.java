7aba926cd07eb1f66f22dbec96b528b82408beeb
Annabdoan <71780695+Annabdoan@users.noreply.github.com>
2022-10-17 17:39:23 +0200

    fix: removed unused variables Co-Authored-By: Maurice mauricew@stud.ntnu.no

--- beer_punishment/json/src/main/java/beerPunishment/json/Persistence.java
+++ beer_punishment/json/src/main/java/beerPunishment/json/Persistence.java
@@ -15,11 +15,9 @@ public class Persistence {
      */
 
     private ObjectMapper mapper = new ObjectMapper().registerModule(createModule());
-    private File ruleFile;
-    private BeerMain beerMain;
 
     public Persistence() {
-        ruleFile = newFile("ruleSaveFile.json");
+
     }
 
     public File newFile(String filename) {
@@ -64,34 +62,9 @@ public class Persistence {
 
     /**
      * Write beermain-object to the saved file.
-     *
      */
     public void writeBeerMain(BeerMain beerMain, File file) throws IOException {
         mapper.writeValue(file, beerMain);
     }
 
-    public static void main(String[] args) throws IOException {
-        Persistence rulepersistence = new Persistence();
-        String fileName = "ruleTest.json";
-        rulepersistence.createFile(fileName);
-        Rule rule1 = new Rule();
-        Rule rule2 = new Rule();
-        rule1.setDescription("Komme for sent");
-        rule1.setPunishmentValue(5);
-        rule2.setDescription("Maurice bannet");
-        rule2.setPunishmentValue(8);
-        BeerMain ruleList = new BeerMain();
-        ruleList.addRule(rule1);
-        ruleList.addRule(rule2);
-        ruleList.addMember("Maurice");
-        ruleList.addMember("Anna");
-        ruleList.punishMember("Anna", rule2);
-        ruleList.punishMember("Anna", rule2);
-        ruleList.punishMember("Maurice", rule1);
-        System.out.println(ruleList);
-        //rulepersistence.writeRule(ruleList, fileName);
-        //BeerMain beerMain3 = rulepersistence.readBeerMain(rulepersistence.ruleFile);
-        //beerMain3.addMember("Lea");
-        //System.out.println(beerMain3);
-    }
 }

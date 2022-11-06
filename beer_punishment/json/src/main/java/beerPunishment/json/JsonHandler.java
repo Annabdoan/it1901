4b8b85aabc233a50b471a03660989150ddb9ef06
saraellingsen <104088161+saraellingsen@users.noreply.github.com>
2022-11-06 15:39:55 +0100

    (#49) Fix: fixed checkstyle warnings in BeerMain and JsonHandler

--- beer_punishment/json/src/main/java/beerPunishment/json/JsonHandler.java
+++ beer_punishment/json/src/main/java/beerPunishment/json/JsonHandler.java
@@ -2,12 +2,13 @@ package beerPunishment.json;
 
 import beerPunishment.core.BeerMain;
 import com.google.gson.Gson;
-
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
+/**
+ * A class for writing and reading from json file.
+ */
 public class JsonHandler {
 
     Gson gson = new Gson();
@@ -21,22 +22,33 @@ public class JsonHandler {
         return System.getProperty("user.home") + filename;
     }
 
+    /**
+     * Writes to JSON.
+     *
+     * @param beerMain the object being written to file
+     * @param filename name of the file
+     */
     public void writeToJson(BeerMain beerMain, String filename) throws IOException {
         try {
             FileWriter fw = new FileWriter(getFilePath(filename));
             gson.toJson(beerMain, fw);
             fw.flush();
             fw.close();
-        }catch (IOException io) {
+        } catch (IOException io) {
             throw new IOException("Feil ved skriving til fil");
         }
     }
 
+    /**
+     * Reads from JSON file and makes it a BeerMain object.
+     *
+     * @param filename the filename to read from
+     */
     public BeerMain readFromJson(String filename) throws IOException {
         try {
             BeerMain beerMain = gson.fromJson(new FileReader(getFilePath(filename)), BeerMain.class);
             return beerMain;
-        }catch (IOException IOe) {
+        } catch (IOException ioe) {
             throw new IOException("Feil ved lesing fra fil");
         }
     }

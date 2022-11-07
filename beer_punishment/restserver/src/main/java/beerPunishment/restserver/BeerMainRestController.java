0e95e6c9c6dd02e98eeecb7e002a03ecdcf44a0b
leaandersenn <89526348+leaandersenn@users.noreply.github.com>
2022-11-07 14:40:21 +0100

    (#57) Feat: Method to write to file in BeerMainRestController Added in a help method to write to file. Implemented this in all POST/PUT/DELETE methods. Co-authored-by: Maurice <mauricew@stud.ntnu.no> Co-authored-by: Anna <abdoan@stud.ntnu.no>

--- beer_punishment/restserver/src/main/java/beerPunishment/restserver/BeerMainRestController.java
+++ beer_punishment/restserver/src/main/java/beerPunishment/restserver/BeerMainRestController.java
@@ -3,9 +3,11 @@ package beerPunishment.restserver;
 import beerPunishment.core.BeerMain;
 import beerPunishment.core.Rule;
 
+import beerPunishment.json.JsonHandler;
 import org.springframework.web.bind.annotation.*;
 
 
+import java.io.IOException;
 import java.util.Collection;
 import java.util.HashMap;
 
@@ -17,6 +19,16 @@ import java.util.HashMap;
 public class BeerMainRestController {
     private BeerMain beerMain = BeerMainService.createBeerMain();
     private Rule rule = new Rule();
+
+    private JsonHandler jsh = new JsonHandler();
+
+    private void writeToJson() {
+        try {
+            jsh.writeToJson(this.beerMain, "/beerPunishment.json");
+        }catch (IOException IOE) {
+            System.out.println("Error while writing to file");
+        }
+    }
 
 
     @GetMapping(path = "beerMain")
@@ -46,11 +58,13 @@ public class BeerMainRestController {
     public void addRule(@RequestParam("description") String ruleDescription, @RequestParam("value") int value) {
         this.rule = new Rule(ruleDescription,value);
         this.beerMain.addRule(rule);
+        writeToJson();
     }
 
     @PostMapping(path = "/addMember")
     public void addMember(@RequestParam("name") String name) {
         this.beerMain.addMember(name);
+        writeToJson();
     }
 
     @PutMapping(path="punishMember")
@@ -59,17 +73,20 @@ public class BeerMainRestController {
                              @RequestParam("value") int value){
         this.rule = new Rule(ruleDescription,value);
         this.beerMain.punishMember(member,this.rule);
+        writeToJson();
     }
 
 
     @DeleteMapping(path = "removeRule")
     public void removeRule(@RequestParam("rule") String ruleDescription){
         this.beerMain.removeRuleUsingDescription(ruleDescription);
+        writeToJson();
     }
 
     @DeleteMapping(path = "deleteMember")
     public void deleteMember(@RequestParam("member") String member) {
         this.beerMain.deleteMember(member);
+        writeToJson();
     }
 
     @DeleteMapping(path = "payPunishment")
@@ -78,5 +95,6 @@ public class BeerMainRestController {
                               @RequestParam("value") int value) {
         this.rule = new Rule(ruleDescription, value);
         this.beerMain.removePunishment(member, rule);
+        writeToJson();
     }
 }

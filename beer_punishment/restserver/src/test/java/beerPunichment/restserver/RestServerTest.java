31a75cd4147e5ec58de04f749e46d1ced5475c57
saraellingsen <104088161+saraellingsen@users.noreply.github.com>
2022-11-11 19:27:51 +0100

    (#66) - Feat: setup for WireMock and pingServer tested

---
+++ beer_punishment/restserver/src/test/java/beerPunichment/restserver/RestServerTest.java
@@ -0,0 +1,10 @@
+package beerPunichment.restserver;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+
+public class RestServerTest {
+
+    private WireMockConfiguration wireMockConfiguration;
+}
--- beer_punishment/ui/src/test/java/beerPunishment/ui/BeerMainRemoteAccessTest.java
+++ beer_punishment/ui/src/test/java/beerPunishment/ui/BeerMainRemoteAccessTest.java
@@ -1,6 +1,64 @@
 package beerPunishment.ui;
 
+import beerPunishment.core.BeerMain;
+import beerPunishment.core.Rule;
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import static org.junit.Assert.assertTrue;
+
 public class BeerMainRemoteAccessTest {
 
+    private WireMockConfiguration wmConfig;
+    private WireMockServer wmServer;
+
     private BeerMainRemoteAccess beerMainRemoteAccess;
+
+    @BeforeEach void setup() throws URISyntaxException {
+        wmConfig = WireMockConfiguration.wireMockConfig().port(8080);
+        wmServer = new WireMockServer(wmConfig.portNumber());
+        wmServer.start();
+        WireMock.configureFor("localhost", wmConfig.portNumber());
+        BeerController beerController = new BeerController();
+        URI defaultURI = URI.create("http://localhost:8080"); //Må lage getter i BeerController!!
+        beerMainRemoteAccess = new BeerMainRemoteAccess(defaultURI);
+
+        final BeerMain beerMain = new BeerMain();
+        final Rule rule = new Rule("Banne", 3);
+        beerMain.addRule(rule);
+        beerMain.addMember("Sara");
+    }
+
+    @Test
+    public void testGetBeerMain() {
+        WireMock.stubFor(WireMock.get(WireMock.urlEqualTo(getUrl()))
+                .withHeader("Accept", WireMock.equalTo("application/json"))
+                .willReturn(WireMock.aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "application/json"))
+
+        );
+
+    }
+
+    private String getUrl(String... segments) {
+        String url = "/beerMain";
+        for (String segment : segments) {
+            url = url + "/" + segment;
+        }
+        return url;
+    }
+
+
 }

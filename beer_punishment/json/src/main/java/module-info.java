6b285d951170b5306790f7504cf1cd27ada7f5c8
mauricew <mauricew@stud.ntnu.no>
2022-11-16 11:36:21 +0100

    (#96) Fix: Changed Gson Version
    Changed from gson from version 2.8.5 to 2.9.1. This version has a built in module-info file in the jar package
    and therefore supports jlink and jpackage

--- beer_punishment/json/src/main/java/module-info.java
+++ beer_punishment/json/src/main/java/module-info.java
@@ -1,14 +1,14 @@
 module beerPunishment.json {
 
     requires beerPunishment.core;
-    requires gson;
+    requires com.google.gson;
     requires java.sql;
     //requires com.fasterxml.jackson.databind;
     //requires com.fasterxml.jackson.annotation;
     //requires com.fasterxml.jackson.core;
     //requires org.json;
 
-    opens beerPunishment.json to gson;
+    opens beerPunishment.json to com.google.gson;
 
     exports beerPunishment.json;
 }

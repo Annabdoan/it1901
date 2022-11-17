40cf8fd7107a33a8648941824c83dc3358eb3fe2
mauricew <mauricew@stud.ntnu.no>
2022-11-17 16:24:45 +0100

    (#104) Fix: Rule Javadoc
    Added javadoc to the methods that were missing it.

--- beer_punishment/core/src/main/java/beerPunishment/core/Rule.java
+++ beer_punishment/core/src/main/java/beerPunishment/core/Rule.java
@@ -9,6 +9,13 @@ public class Rule {
     private int punishmentValue;
 
     /**
+     * Empty constructor needed for test.
+     */
+    public Rule() {
+
+    }
+
+    /**
      * Creates Rule with all relevant parameters.
      *
      * @param description     description of rule
@@ -49,13 +56,6 @@ public class Rule {
         return true;
     }
 
-    /**
-     * Empty constructor needed for test.
-     */
-    public Rule() {
-
-    }
-
     public String getDescription() {
         return description;
     }
@@ -64,6 +64,13 @@ public class Rule {
         return punishmentValue;
     }
 
+    /**
+     * A simple equals method that compares two Rules based only on the description.
+     *
+     * @param r2 The rule to compare with this rule.
+     *
+     * @return Returns a boolean. True if equal, false else.
+     */
     public Boolean equals(Rule r2) {
         return r2.getDescription().equalsIgnoreCase(this.getDescription());
     }
@@ -72,6 +79,11 @@ public class Rule {
         return String.format("[Rule rule=%s value=%s]", getDescription(), getPunishmentValue());
     }
 
+    /**
+     * A seperate toString method used to generate a prettier print that is used in the UI.
+     *
+     * @return The rule in a "pretty print"  format.
+     */
     public String toStringDisplayFormat() {
         return description + "\t\t\t" + punishmentValue;
     }

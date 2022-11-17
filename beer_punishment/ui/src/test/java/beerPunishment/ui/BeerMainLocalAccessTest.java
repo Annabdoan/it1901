1f0b49a4419985091d9e4a4cbb8b6e56a67ca830
mauricew <mauricew@stud.ntnu.no>
2022-11-17 19:10:37 +0100

    (#98) Feat: BeforeEach LocalAccessTest
    Added two private fields and a BeforeEach method to setup the testenvironement

--- beer_punishment/ui/src/test/java/beerPunishment/ui/BeerMainLocalAccessTest.java
+++ beer_punishment/ui/src/test/java/beerPunishment/ui/BeerMainLocalAccessTest.java
@@ -1,4 +1,20 @@
 package beerPunishment.ui;
 
+import beerPunishment.core.BeerMain;
+import org.junit.jupiter.api.BeforeEach;
+
+/**
+ * BeerControllerTest test most of the methods in BeerMainLocalAccess, although it misses some branches.
+ * This testclass aims to test those missed branches to improve our testcoverage.
+ */
+
 public class BeerMainLocalAccessTest {
+    private BeerMain bm;
+    private BeerMainLocalAccess bmLA;
+
+    @BeforeEach
+    public void setup() {
+        bm = new BeerMain();
+        bmLA = new BeerMainLocalAccess();
+    }
 }

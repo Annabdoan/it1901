594631ad7bc9fa0e21c0a66123f4142d8dfefd63
mauricew <mauricew@stud.ntnu.no>
2022-11-17 20:48:39 +0100

    (#98) Feat: Exception testing
    Added tests for testing that methods in BeerMainLocalAccess throws exception where expected

--- beer_punishment/ui/src/test/java/beerPunishment/ui/BeerMainLocalAccessTest.java
+++ beer_punishment/ui/src/test/java/beerPunishment/ui/BeerMainLocalAccessTest.java
@@ -1,7 +1,11 @@
 package beerPunishment.ui;
 
 import beerPunishment.core.BeerMain;
+import beerPunishment.core.Rule;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
  * BeerControllerTest test most of the methods in BeerMainLocalAccess, although it misses some branches.
@@ -16,5 +20,19 @@ public class BeerMainLocalAccessTest {
     public void setup() {
         bm = new BeerMain();
         bmLA = new BeerMainLocalAccess();
+    }
+
+    @Test
+    public void testThrowsIOException() {
+        bmLA.changeLocalFilename(null);
+        assertDoesNotThrow(() -> bmLA.getBeermain());
+        assertDoesNotThrow(() -> bmLA.addRule(bm, "Test", 1));
+        bm.addRule(new Rule("Test", 1));
+        assertThrows(IllegalArgumentException.class, () -> bmLA.deleteRule(null, "Test"));
+        assertDoesNotThrow(() -> bmLA.addMember(bm, "Test"));
+        bm.addMember("Test");
+        assertThrows(IllegalArgumentException.class, () -> bmLA.deleteMember(bm, "Test"));
+        assertThrows(IllegalArgumentException.class, () -> bmLA.punishMember(bm, "Test", "TestRule", 1));
+        assertThrows(IllegalArgumentException.class, () -> bmLA.deletePunishment(bm, "Test", "TestRule", 1));
     }
 }

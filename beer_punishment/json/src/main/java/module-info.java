4020cefab9fde0bf2d0c1242e32a645ba6747b9d
Annabdoan <71780695+Annabdoan@users.noreply.github.com>
2022-11-17 15:51:20 +0100

    (#106) fix: delete comments

--- beer_punishment/json/src/main/java/module-info.java
+++ beer_punishment/json/src/main/java/module-info.java
@@ -3,10 +3,6 @@ module beerPunishment.json {
     requires beerPunishment.core;
     requires com.google.gson;
     requires java.sql;
-    //requires com.fasterxml.jackson.databind;
-    //requires com.fasterxml.jackson.annotation;
-    //requires com.fasterxml.jackson.core;
-    //requires org.json;
 
     opens beerPunishment.json to com.google.gson;
 
